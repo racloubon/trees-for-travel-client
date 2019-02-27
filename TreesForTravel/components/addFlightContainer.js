3b289b522d4f2992bc81bb97dadcb96bbb195ca1
Rachel Bonny <racloubon@gmail.com>
2019-02-27 21:40:31 +0100

    city lookup and calculations: distance, carbon footprint, trees needed to offset

--- TreesForTravel/components/addFlightContainer.js
+++ TreesForTravel/components/addFlightContainer.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 import Forest from './forest.js';
 
+import Cities from '../data/cities.json'
+
 export default class addFlightContainer extends React.Component {
 
   constructor(props) {
@@ -14,32 +16,63 @@ export default class addFlightContainer extends React.Component {
      };
    }
 
+   //original below at: https://www.movable-type.co.uk/scripts/latlong.html
+   //found at: https://stackoverflow.com/questions/5260423/torad-javascript-function-throwing-error
+   calculateDistanceInKm = (lat1, lon1, lat2, lon2) => {
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a =
+        0.5 - Math.cos(dLat)/2 +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        (1 - Math.cos(dLon))/2;
+
+    return R * 2 * Math.asin(Math.sqrt(a));
+   }
+
+   //refs: https://www.carbonindependent.org/sources_aviation.html
 
   addFlight = () => {
     const origin = this.state.origin;
     const destination = this.state.destination;
 
+    // console.log(Cities.find(data => data.city === origin).lat)
+    // console.log(Cities.find(data => data.city === origin).lng)
+    // console.log(Cities.find(data => data.city === destination).lat)
+    // console.log(Cities.find(data => data.city === destination).lng)
+
+
+    //currently has default co-ordinates for London to New York - need to implement database to look-up
+    const orLat = Cities.find(data => data.city === origin).lat;
+    const orLon = Cities.find(data => data.city === origin).lng;
+    const desLat = Cities.find(data => data.city === destination).lat;
+    const desLong = Cities.find(data => data.city === destination).lng;
+    const distance = Math.trunc(this.calculateDistanceInKm(orLat, orLon, desLat, desLong))
+    const carbonFootprint = distance*115 //in grams
+    const treesNeeded = carbonFootprint/900000 //in grams, assuming one tree absorbs 1 ton in its lifetime
+    const treesNeededRounded = Math.round(treesNeeded)*10 //currently one tree = 1/10th of a tree because otherwise they're too small
+
     if (origin && destination) {
-      console.log(origin, destination)
-      //calculate everything here
-    }
+      console.log(origin, destination, distance, treesNeeded)
 
-    const newFlight = {
-      origin: this.state.origin,
-      destination: this.state.destination,
-      distance: 0,
-      carbonFootprint: 0,
-      treesToOffset: [0,0,0]
-    }
+      const newFlight = {
+        origin: origin,
+        destination: destination,
+        distance: distance,
+        carbonFootprint: carbonFootprint,
+        treesToOffset: new Array(treesNeededRounded).fill(0)
+      }
 
-    let flightsArray = [...this.state.flights];
-    flightsArray.push(newFlight);
-    this.setState({flights: flightsArray});
+      console.log(newFlight)
+
+      let flightsArray = [...this.state.flights];
+      flightsArray.push(newFlight);
+      this.setState({flights: flightsArray});
+    }
   }
 
   render() {
       return <View style={{flex: 6, backgroundColor: '#D8DAD3'}}>
-        <Text>Add a Flight Container</Text>
         <TextInput
           style={{height: 40, backgroundColor: "#F1F2EB", margin: 10}}
           placeholder="From"
--- TreesForTravel/components/navSlider.js
+++ TreesForTravel/components/navSlider.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class navSlider extends React.Component {
 
   render() {
-      return <View style={{flex: 0.8, backgroundColor: '#A4C2A3'}}><Text>Nav Slider</Text></View>
+      return <View style={{flex: 1, backgroundColor: '#A4C2A3'}}><Text>Nav Slider</Text></View>
   }
 
 }
